6f821d4603cc8778ffdb87cc18335771d1673d94
JTor <t.jor13@gmail.com>
2022-08-23 12:48:18 -0400

    This exercises passes all tests, removed pseudocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,6 @@
 const removeFromArray = function(array, ...args) {
-    //console.log(array.includes(args));
     return array.filter((x) => !args.includes(x));
 };
-
-/*
-for each element in array
-    check if element is equal to the other arguments in function
-    if equal, remove element from array
-    continue through to next elements
-return
-
-*/
-
-
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
