549b37d6774e3c6153d7c0f8baf7478f8f36b956
JTor <t.jor13@gmail.com>
2022-08-23 11:37:48 -0400

    Exercise passes first test, included pseudocode

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+/*
+take in string argument
+split string and assign it to text var
+reverse string array
+rejoin array values into string
+return the string
+*/
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
