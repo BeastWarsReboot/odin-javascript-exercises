97f437aa599e0ab42a53352d51bebbbdcfeb29b7
JTor <t.jor13@gmail.com>
2022-08-23 10:43:52 -0400

    Exercise passes first two tests thus far

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result='';
+    while(num){
+        result += str;
+        num--;
+    }
+    console.log(result);
+    return result;
+
+//declare and define empty string result var
+//while num is true
+//concat result with str
+//decrement num
+//when num is false, return result
 
 };
 
