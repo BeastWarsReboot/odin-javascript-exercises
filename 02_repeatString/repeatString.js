5db10d181afdc42cfebc20e68aa566971dd6f3e8
JTor <t.jor13@gmail.com>
2022-08-23 11:02:36 -0400

    Added error for negative number, cleaned up pseudocode, enabled tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,18 +1,11 @@
 const repeatString = function(str, num) {
     let result='';
     while(num){
+        if(num < 0) return 'ERROR';
         result += str;
         num--;
     }
-    console.log(result);
     return result;
-
-//declare and define empty string result var
-//while num is true
-//concat result with str
-//decrement num
-//when num is false, return result
-
 };
 
 // Do not edit below this line
