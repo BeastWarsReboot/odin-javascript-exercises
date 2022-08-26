9a37a7f896710fb9773f8ed7c583bd32ad25dfa8
JTor <t.jor13@gmail.com>
2022-08-26 09:50:56 -0400

    This exercise passes the first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //from fahrenheit
+  //C = (F - 32) * .556
+  return Math.floor((temp - 32) * .556);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  //from celsius
+  //F = C * 1.8 + 32
+  return Math.floor(temp * 1.8 + 32);
 };
 
 // Do not edit below this line
