e445e99bf791a855bcc216f7077fc45823c63caf
JTor <t.jor13@gmail.com>
2022-08-31 09:52:28 -0400

    Sum passes tests, beginning work on multiplication

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,20 @@
-const add = function(a,b) {
+const add = function(a, b) {
   return a + b;
 };
 
-const subtract = function(a,b) {
+const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
 
 };
 
