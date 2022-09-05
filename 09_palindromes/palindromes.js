8ec6af7752bd26976fc977369abfa3c5d19ca8e7
JTor <t.jor13@gmail.com>
2022-09-05 19:27:41 -0400

    Code passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,6 @@
 const palindromes = function (str) {
-    /*
-    create variable to hold "new" string
-    remove punctuation marks(such as commas or exclamation points) using regex
-    split string into array
-    reverse array
-    join array back into string
-
-    */
-    console.log(str);
-    let result = str.replace(/[^\w\s\']|_/g, "")
-                    .replace(/\s+/g, " ")
-                    .split()
-                    .reverse()
-                    .join();
-    str = str.replace(/[^\w\s\']|_/g, "");
-    console.log(result);
-    if(result === str) return true;
-    else return false;
+    let result = str.toLowerCase().replace(/[^a-z]/g, "");
+    return result.split("").reverse().join("") == result;
 };
 
 // Do not edit below this line
