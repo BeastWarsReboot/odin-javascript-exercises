1ba260ae10631d492716c3d3c86e6358d7078061
JTor <t.jor13@gmail.com>
2022-09-06 10:59:43 -0400

    Code passes all tests, cleaned up for readability

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,31 +2,14 @@ const fibonacci = function(n) {
     n = parseInt(n);
     if(n < 1) return 'OOPS';
     else {
-        let count = 1;
-        let n1 = 0, n2 = 1, fib;
+        let count = 1, n1 = 0, n2 = 1, fib = 1;
         while(count < n){
             fib = n1 + n2;
             n1 = n2;
             n2 = fib;
-            console.log(fib);
             count++;
-        }
-
-        return fib;
+        } return fib;
     }
-    /*
-    parseInt to make string a number if it isnt yet
-    if n is less than 1, error message
-    otherwise create var for count, first term, second term and fib number
-    while count is less than n
-        fib equals sum of first and second term
-        term one is equal to term two
-        term two is equal to fib
-        increment counter
-        when count is equal to n, exit
-    return fib sequence number
-    */
-
 };
 
 
