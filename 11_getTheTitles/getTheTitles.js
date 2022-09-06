8f8afd0b27ca59485eb068735406d3e1e49e9cf3
JTor <t.jor13@gmail.com>
2022-09-06 11:30:56 -0400

    Removed pseudocode

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -2,11 +2,6 @@ const getTheTitles = function(books) {
     const titles = [];
     books.forEach(books => titles.push(books.title));
     return titles;
-    /*
-        create array to hold titles
-        for each book in array
-            push title to array of titles
-    */
 };
 
 // Do not edit below this line
