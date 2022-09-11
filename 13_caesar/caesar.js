8590aa6f78ebc490cb1cb0e7708d28401918c673
JTor <t.jor13@gmail.com>
2022-09-10 22:50:36 -0400

    Cleaned up all the rough draft comments, should look a bit cleaner now

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,89 +1,41 @@
 const caesar = function(str, shift) {
-    /*
-    create array of alphabet
-    convert characters of str to unicode?
-    for each element of array
-        use the shift number to shift the letters of the string/array
-        to get the deciphered values of the letters
-        add the values to result 
-    return the deciphered result string
-    */
 
-    //will hold enciphered message
-    let upperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let lowerAlpha = upperAlpha.toLowerCase();
-   
+    //alphabet for finding characters
+    //result for holding the enciphered message
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let result = "";
 
-    //for loop starts by getting unicode values on each character in the string
-    //then adds the shift value to the character code
+    //for loop adds charIndex to shift modulo 26 to get the charShift value 
+    //Shifted Character = (OG character index + shift) % 26
     for(let i = 0; i < str.length; i++){
-        //let charIndex = str.indexOf(i);
-        let charIndex = upperAlpha.indexOf(str.charAt(i).toUpperCase());
-        //console.log(str.charAt(i));
-        //console.log(charIndex);
-        let charShift = (charIndex + (shift)) % 26;
+        let charIndex = alphabet.indexOf(str.charAt(i).toUpperCase());
+        let charShift = (charIndex + shift) % 26;
+
+        //if charShift is a negative value, add 26 to get the proper enciphered character
         if(Math.sign(charShift) === -1){
             charShift += 26;
         }
-        //console.log(charShift);
+
         
-        //Trying to get this to deal with wrapping
-        //let wrapChk = charCode % charShift;
-        //92 % 87 = 5
-        //Shifted Character = (OG character index + shift) % 26
-        //!upperAlpha.includes(str.charAt(i)) && !lowerAlpha.includes(str.charAt(i))
+        //This checks if the current character is a punctuation mark (which eval to -1)
+        //then adds them to the message unchanged
         if(charIndex == -1){
             result += str.charAt(i);
         }
-  
-        //this deals with punctuation
-        /*
-        if(/[!?. ,]/.test(str.charAt(i)) == true){
-            result += str.charAt(i);
-        }
-        */
         
-        //This addes the enciphered characters to the result
-        else if(upperAlpha.includes(str.charAt(i))){
-            result += upperAlpha.charAt(charShift);
+        //Otherwise this addes the enciphered characters to the result
+        //by checking if they are included in the alphabe with some casing work
+        else if(alphabet.includes(str.charAt(i))){
+            result += alphabet.charAt(charShift);
         }
-        else if (lowerAlpha.includes(str.charAt(i))){
-            result += lowerAlpha.charAt(charShift);
+        else if (alphabet.toLowerCase().includes(str.charAt(i))){
+            result += alphabet.toLowerCase().charAt(charShift);
         }
-        /*
-        else if(charShift >= 97 && charShift <= 122){
-            result += String.fromCharCode(charShift);
-        } else if(charShift >= 65 && charShift <= 90){
-            result += String.fromCharCode(charShift);
-        }
-        */
-        //result += String.fromCharCode(charShift)
-        //console.log(charShift);
     }
+
     console.log(result);
     return result;
-//uppercase letters unicode: 65 - 90
-//lowercase letters unicode: 97 - 122
-
-
 };
-
-/*
-
-> In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, 
-Caesar's code or Caesar shift, is one of the simplest and most widely known 
-encryption techniques. It is a type of substitution cipher in which each letter in 
-the plaintext is replaced by a letter some fixed number of positions down the 
-alphabet.  For example, with a left shift of 3, D would be replaced by A, E would 
-become B, and so on. The method is named after Julius Caesar, who used it in his 
-private correspondence.
-
-Hint: You may need to convert letters to their unicode values. Be sure to read the 
-documentation!
-
-*/
-
 
 
 // Do not edit below this line
